6b42c12a9a6212a6a4876c6a0a18862a6ccb6fcd
Pablo Gonzalez <pablo.alejandro315@gmail.com>
2022-01-10 19:04:36 -0300

    navbar

---
+++ src/components/Navbar/Navbar.js
@@ -0,0 +1,15 @@
+function Navbar () {
+    return <nav>
+        <ul>
+            <li>Nosotros</li>
+            <li>Tienda</li>
+            <li>Contacto</li>
+            
+        </ul>
+        
+        
+        
+        </nav>
+}
+
+export default Navbar;
