5c0aa3e240935f0d2af10e80128e640a32d16ac0
Pablo Gonzalez <pablo.alejandro315@gmail.com>
2022-01-18 21:59:54 -0300

    Counter

---
+++ src/components/Counter/ItemCounter.js
@@ -0,0 +1,30 @@
+import React from "react";
+import { useState } from "react";
+import "./ItemCounter.css";
+
+const ItemCounter = () => {
+    const [count, setCount] = useState(0);
+    const contar = () => {
+        setCount(count + 1);
+    };
+    const restar = () => {
+        setCount(count - 1);
+    };
+
+    if (count < 0) {
+        return <p className="error">No puedes tener una cantidad negativa</p>;
+    }
+
+    return (
+        <div className="titulo3">
+            <h4>Agregar</h4>
+            <div className="contador">
+            <button onClick={restar}>-</button>
+            <p>{count}</p>
+            <button onClick={contar}>+</button>
+            </div>
+        </div>
+    );
+};
+
+export default ItemCounter;
